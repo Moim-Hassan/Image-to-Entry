b65df86b4bd65b8b97ec40ce2d1c7ebdfdb44c4c
MD. KHAN MAHMUD MAIM HASSAN <moim.hassan@gmail.com>
2026-04-19 09:16:45 +0600

    changed ai model

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ if a:
         with st.spinner('AI is generating the image details'):
 
             res = cl.models.generate_content(
-            model='gemini-3-flash-preview',
+            model='Gemini 3.1 Flash Lite',?
             contents=[prompt,*pimg]
        )
             # st.markdown(res.text)
